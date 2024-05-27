7a0d1827ebd45d0ad745b0733ddfde8be690960a
myimer <myimer@eagle.org>
2024-05-27 10:31:27 -0500

    update all

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Jupyter_Workspace\myproject\venv
+set VIRTUAL_ENV=C:\JupyterWorkspace\Jupyter_Workspace\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
